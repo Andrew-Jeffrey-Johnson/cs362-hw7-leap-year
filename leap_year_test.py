a9bf35798dd0ce80119deec72c119f727e5e0326
johnand4 <63374727+johnand4@users.noreply.github.com>
2021-05-28 19:55:48 -0700

    Wrote test checking if 100 is not a leap year
    This test checks if the year 100 is not a leap year.

--- leap_year_test.py
+++ leap_year_test.py
@@ -5,5 +5,8 @@ class testCaseLeapYear(unittest.TestCase):
 	def test_4(self):
 		self.assertEqual(leap_year.leap_year(4), True)
 
+	def test_100(self):
+		self.assertEqual(leap_year.leap_year(100), False)
+
 	if __name__ == '__main__':
 		unittest.main()
